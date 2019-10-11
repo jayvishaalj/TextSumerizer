b9b28c8ea27f09e3925b6b9208d67e4e38dd153f
Harshana-S <hana.8500@gmail.com>
2019-10-12 02:02:58 +0530

    Initial Commit for SRMHACK

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 from flask import Flask ,jsonify,request
 app = Flask(__name__)
 
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
 @app.route('/api/getAdventurePlaces', methods=['POST'])
 def hello():
     req = request.get_json()
@@ -27,8 +11,8 @@ def hello():
     
     places = [
 	    {   
-		    'locationNames': ['Place1','Place2','Place3']
-		
+		    'locationNames': ['Place1','Place2','Place3'],
+            'locationDescription' : ['Description1','Description2','Description3']		
 	    }
     ]
     return jsonify({'places': places})
