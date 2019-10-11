1f5571a06b4bf771a89977ae154fbd047eb28f0c
Harshana-S <hana.8500@gmail.com>
2019-10-12 04:43:05 +0530

    Pickle Api for adventure traveller added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
-@app.route('/home',methods=['POST'])
+@app.route('/home',methods=['GET'])
 def home():
     req = request.get_json()
     userId = req['userId']
