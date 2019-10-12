8adfa0b88c5c593be2e100a9a524fd3035b80d17
Harshana-S <hana.8500@gmail.com>
2019-10-12 05:53:58 +0530

    Pickle Api for adventure traveller added

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def homepage():
 
 @app.route('/home')
 def home():
-    return jsonify({'places': ['Place1','Place2','Place3']})
+    return jsonify({
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+})
 
 
 # load model
